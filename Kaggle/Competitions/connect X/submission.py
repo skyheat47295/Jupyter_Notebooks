3084b1acbd2c07a11bae82a65e2da906dd2e2ff3
skyheatfet <70936076+skyheatfet@users.noreply.github.com>
2021-04-21 23:20:59 -0700

    4.21.21.1

---
+++ Kaggle/Competitions/connect X/submission.py
@@ -0,0 +1,120 @@
+def my_agent(obs, config):
+    # Your code here: Amend the agent!
+    ################################
+    # Imports and helper functions #
+    ################################
+    
+    import numpy as np
+    import random
+
+    # Gets board at next step if agent drops piece in selected column
+    def drop_piece(grid, col, piece, config):
+        next_grid = grid.copy()
+        for row in range(config.rows-1, -1, -1):
+            if next_grid[row][col] == 0:
+                break
+        next_grid[row][col] = piece
+        return next_grid
+
+    # Returns True if dropping piece in column results in game win
+    def check_winning_move(obs, config, col, piece):
+        # Convert the board to a 2D grid
+        grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+        next_grid = drop_piece(grid, col, piece, config)
+        # horizontal
+        for row in range(config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(next_grid[row,col:col+config.inarow])
+                if window.count(piece) == config.inarow:
+                    return True
+        # vertical
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns):
+                window = list(next_grid[row:row+config.inarow,col])
+                if window.count(piece) == config.inarow:
+                    return True
+        # positive diagonal
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(next_grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+                if window.count(piece) == config.inarow:
+                    return True
+        # negative diagonal
+        for row in range(config.inarow-1, config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(next_grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+                if window.count(piece) == config.inarow:
+                    return True
+        return False
+    
+    
+        # Calculates score if agent drops piece in selected column
+    def score_move(grid, col, mark, config):
+        next_grid = drop_piece(grid, col, mark, config)
+        score = get_heuristic(next_grid, mark, config)
+        return score
+    # Helper function for score_move: gets board at next step if agent drops piece in selected column
+    def drop_piece(grid, col, mark, config):
+        next_grid = grid.copy()
+        for row in range(config.rows-1, -1, -1):
+            if next_grid[row][col] == 0:
+                break
+        next_grid[row][col] = mark
+        return next_grid
+    # Helper function for score_move: calculates value of heuristic for grid
+    def get_heuristic(grid, mark, config):
+        num_threes = count_windows(grid, 3, mark, config)
+        num_fours = count_windows(grid, 4, mark, config)
+        num_threes_opp = count_windows(grid, 3, mark%2+1, config)
+        score = num_threes - 1e2*num_threes_opp + 1e6*num_fours
+        return score
+    # Helper function for get_heuristic: checks if window satisfies heuristic conditions
+    def check_window(window, num_discs, piece, config):
+        return (window.count(piece) == num_discs and window.count(0) == config.inarow-num_discs)
+
+    # Helper function for get_heuristic: counts number of windows satisfying specified heuristic conditions
+    def count_windows(grid, num_discs, piece, config):
+        num_windows = 0
+        # horizontal
+        for row in range(config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[row, col:col+config.inarow])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # vertical
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns):
+                window = list(grid[row:row+config.inarow, col])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # positive diagonal
+        for row in range(config.rows-(config.inarow-1)):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row+config.inarow), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        # negative diagonal
+        for row in range(config.inarow-1, config.rows):
+            for col in range(config.columns-(config.inarow-1)):
+                window = list(grid[range(row, row-config.inarow, -1), range(col, col+config.inarow)])
+                if check_window(window, num_discs, piece, config):
+                    num_windows += 1
+        return num_windows
+
+    
+    valid_moves = [col for col in range(config.columns) if obs.board[col] == 0]
+    # Your code here: Amend the agent!
+    for col in valid_moves:
+        if check_winning_move(obs, config, col, obs.mark):
+            return col
+        elif check_winning_move(obs, config, col, obs.mark%2+1):
+            return col
+        
+    # Convert the board to a 2D grid
+    grid = np.asarray(obs.board).reshape(config.rows, config.columns)
+    # Use the heuristic to assign a score to each possible board in the next turn
+    scores = dict(zip(valid_moves, [score_move(grid, col, obs.mark, config) for col in valid_moves]))
+    # Get a list of columns (moves) that maximize the heuristic
+    max_cols = [key for key in scores.keys() if scores[key] == max(scores.values())]
+    # Select at random from the maximizing columns
+    return random.choice(max_cols)
